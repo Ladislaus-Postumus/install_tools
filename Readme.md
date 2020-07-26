ce29d7640f67b1f868ebc6029d231d5ca4c63dc9
Philipp Melzer <philipp.melzer2014@gmail.com>
2020-07-26 17:53:13 +0200

    initial commit: created Script + Readme

---
+++ Readme.md
@@ -0,0 +1,40 @@
+Readme
+======
+
+This is a Powershell-Script which will install tools for you.
+The tools are install with the Chocolatey package manager
+If a tool is already installed with Choco it will be updated.
+
+The Script will elevate itself to install everything.
+
+Currently included tools:
+* Chocolatey
+* Firefox
+* git
+* Neovim
+* Gradle
+* Groovy
+* Python 3 & 2
+* uTorrent
+* Paint.Net
+
+Commandline Arguments
+---------------------
+
+* `less` - will only install Choco and Firefox
+* `vim` - swaps Neovim for Vim
+* `neovim` - installs neovim despite `less`
+* `git` - installs git despite `less`
+* `Paint.Net` - installs Paint.Net despite `less`
+* `torrent` - installs uTorrent despite `less`
+
+TODO
+----
+
+- [ ] install (newest) JDK
+- [ ] install different Versions of JRE
+
+For the Oracle version not possible with Choco.
+
+- [ ] get Vim settings from GitHub
+- [ ] install Vim Plugins
---
+++ install_tools.ps1
@@ -0,0 +1,61 @@
+﻿# Self-elevate the script if required
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+  $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+  Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+  Exit
+}
+
+# Install chocolatey
+if (-Not (choco --version)) {
+  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+} else {
+  choco upgrade chocolatey -y
+}
+
+# Create list of all packages to install
+if (-Not ($args -contains "less")) {
+  $packages = @("neovim", "git", "python", "python2", "firefox", "gradle", "groovy", "utorrent", "paint.net")
+} else {
+  $packages = @("firefox")
+}
+foreach ($arg in $args) {
+  switch ($arg) {
+    "node" { $packages += @("nodejs", "yarn") }
+    "vim" {
+      $packages = $packages | where {$_ -ne "neovim"}
+      $packages += "vim"
+    }
+    "neovim" {
+      if (-Not ($packages -contains "neovim")) {
+        $packages += "neovim"
+      }
+    }
+    "torrent" {
+      $packages += "utorrent"
+    }
+    "git" {
+      $packages += "git"
+    }
+    "paint" {
+      $packages += "paint.net"
+    }
+  }
+}
+
+# Install Choco Packages
+foreach ($package in $packages) {
+  if (-Not (choco list -localonly | Select-String -Pattern $package)) {
+    choco install $package -y
+  } else {
+    choco upgrade $package -y
+  }
+}
+
+# TODO:
+# Install Docker?
+# not with choco
+# Install TeX
+# Install JDK (newest)
+# Install JRE (1.8/ 1.11)
+
+sleep 10
